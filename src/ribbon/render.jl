e3d8f87237082c1b95ab28613d80c8f53a827a4e
anton083 <anton.oresten42@gmail.com>
2024-05-19 13:44:16 +0200

    Reuse render! function & residue color fix

--- src/ribbon/render.jl
+++ src/ribbon/render.jl
@@ -1,57 +1,40 @@
 include("shapes/shapes.jl")
 
 function get_surface_vertices(ribbon::Ribbon, ::Val{:Coil}, segment_range::UnitRange{Int}, chain::Protein.Chain)
-    adjusted_start = max(1, segment_range.start - 1)
-    adjusted_end = min(length(chain), segment_range.stop + 1)
+    adjusted_range = max(1, segment_range.start - 1):min(length(chain), segment_range.stop + 1)
     points = Protein.alphacarbon_coords(chain)
-    return coil_surface(ribbon.attributes, points; segment_range=adjusted_start:adjusted_end)
+    return coil_surface(ribbon.attributes, points; segment_range=adjusted_range), adjusted_range
 end
 
 function get_surface_vertices(ribbon::Ribbon, ::Val{:Helix}, segment_range::UnitRange{Int}, chain::Protein.Chain)
     points = Protein.alphacarbon_coords(chain)
-    return helix_surface(ribbon.attributes, points; segment_range)
+    return helix_surface(ribbon.attributes, points; segment_range), segment_range
 end
 
 function get_surface_vertices(ribbon::Ribbon, ::Val{:Strand}, segment_range::UnitRange{Int}, chain::Protein.Chain)
     ca_points = Protein.alphacarbon_coords(chain)
     o_points = Protein.oxygen_coords(chain) .|> eltype(ca_points)
-    return strand_surface(ribbon.attributes, ca_points, o_points; segment_range)
+    return strand_surface(ribbon.attributes, ca_points, o_points; segment_range), segment_range
 end
 
-function render!(
-    ribbon::Ribbon,
-    chain::Protein.Chain,
-    color::AbstractVector{<:Real},
-)
-    length(chain) > 1 || throw(ArgumentError("Chain must have at least 2 residues"))
+function render!(ribbon::Ribbon, chain::Protein.Chain, color::AbstractVector{<:Real}, colormap)
     for (ss_name, segment_range) in segments(chain)
-        surface_vertices = get_surface_vertices(ribbon, Val(ss_name), segment_range, chain)
+        surface_vertices, adjusted_range = get_surface_vertices(ribbon, Val(ss_name), segment_range, chain)
         surface!(ribbon, eachslice(surface_vertices, dims=1)...,
-            color=expand_colors(color[segment_range], size(surface_vertices, 2)), colormap=ribbon.colormap, colorrange=(0,1))
+            color = expand_colors(color[adjusted_range], size(surface_vertices, 2)),
+            colormap = colormap,
+            colorrange = (0, 1))
     end
     return ribbon
 end
 
-function render!(
-    ribbon::Ribbon,
-    chain::Protein.Chain,
-    color::AbstractVector{<:Colorant},
-)
-    length(chain) > 1 || throw(ArgumentError("Chain must have at least 2 residues"))
-    for (ss_name, segment_range) in segments(chain)
-        surface_vertices = get_surface_vertices(ribbon, Val(ss_name), segment_range, chain)
-        surface!(ribbon, eachslice(surface_vertices, dims=1)...,
-                 color=expand_colors(color[segment_range], size(surface_vertices, 2)))
-    end
-    return ribbon
+function render!(ribbon::Ribbon, chain::Protein.Chain, color::AbstractVector{<:Colorant}, colormap)
+    render!(ribbon, chain, LinRange(0, 1, length(color)), color)
 end
 
 function draw_lines_between_subchains!(
-    ribbon::Ribbon,
-    chain::Protein.Chain,
-    subchain_ranges::Vector{UnitRange{Int}},
-    color::Symbol;
-    linewidth=2,
+    ribbon::Ribbon, chain::Protein.Chain, subchain_ranges::Vector{UnitRange{Int}},
+    color, linewidth = 2
 )
     subchains = [chain[r] for r in subchain_ranges]
     for (i, j) in zip(1:length(subchains)-1, 2:length(subchains))
@@ -64,10 +47,12 @@ function draw_lines_between_subchains!(
 end
 
 function render!(ribbon::Ribbon, chains::Vector{Protein.Chain})
-    for (chain, colors) in zip(chains, ribbon.colors[])
+    for (chain, color) in zip(chains, ribbon.colors[])
+        length(chain) > 1 || throw(ArgumentError("Chain must have at least 2 residues"))
+        length(chain) == length(color) || throw(ArgumentError("Chain and color must have the same length"))
         subchain_ranges = get_subchain_ranges(chain)
         for r in subchain_ranges
-            render!(ribbon, chain[r], colors[r])
+            render!(ribbon, chain[r], color[r], ribbon.colormap)
         end
         draw_lines_between_subchains!(ribbon, chain, subchain_ranges, :lightgray)
     end
