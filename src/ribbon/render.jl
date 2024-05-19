6e2e63139eab898f7888ecc14a25e8ef26e69aad
anton083 <anton.oresten42@gmail.com>
2024-05-19 21:23:57 +0200

    LinRange -> range

--- src/ribbon/render.jl
+++ src/ribbon/render.jl
@@ -40,7 +40,7 @@ function draw_lines_between_subchains!(
     for (i, j) in zip(1:length(subchains)-1, 2:length(subchains))
         startpoint, endpoint = subchains[i].backbone[end-1], subchains[j].backbone[begin+1]
         n_segments = trunc(Int, norm(endpoint - startpoint) / 0.8)
-        xs, ys, zs = [LinRange(startpoint[i], endpoint[i], 2*n_segments) for i in 1:3]
+        xs, ys, zs = [range(startpoint[i], endpoint[i], 2*n_segments) for i in 1:3]
         linesegments!(ribbon, xs, ys, zs; linewidth=linewidth, color=color, transparency=true)
     end
     return ribbon
