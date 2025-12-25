694a913088fad1b8caea3a522bddab28993eab57
BenjaminDoran <20494558+BenjaminDoran@users.noreply.github.com>
2025-12-25 15:04:59 -0600

    update function signitures for computegraph

--- src/ribbon/shapes/strand.jl
+++ src/ribbon/shapes/strand.jl
@@ -38,7 +38,7 @@ function alternating_normal_correction!(normals::AbstractMatrix{T}) where T <: R
 end
 
 function strand_surface(
-    attributes::Attributes,
+    attributes::Union{Attributes, Makie.ComputeGraph},
     segment_range::UnitRange{Int},
     all_ca_points::AbstractMatrix{T},
     all_o_points::AbstractMatrix{T},
